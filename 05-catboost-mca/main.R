d18d1171605bff74b5f1cc0097a8737e549d4bee
Nikolay Markov <a4blank@yahoo.com>
2018-05-16 20:19:46 +0100

    Use cell type annotations

--- 05-catboost-mca/main.R
+++ 05-catboost-mca/main.R
@@ -1,11 +1,13 @@
 require(Seurat)
 require(scmap)
 
-SCE_CACHE_DIR <- file.path(dirname(dirname(sys.frame(1)$ofile)), '01-cluster-sc01-sc02')
-CACHE_DIR <- dirname(sys.frame(1)$ofile)
+CURRENT_DIR <- dirname(sys.frame(1)$ofile)
+CODE_ROOT <- dirname(CURRENT_DIR)
+SCE_CACHE_DIR <- file.path(CODE_ROOT, '01-cluster-sc01-sc02')
+METADATA_DIR <- file.path(CODE_ROOT, '00-metadata')
 
 pdfPlot <- function(filename, plot, width=8, height=6) {
-  pdf(file=file.path(CACHE_DIR, filename))
+  pdf(file=file.path(CURRENT_DIR, filename))
   result <- plot()
   dev.off()
   return(result)
@@ -13,7 +15,12 @@ pdfPlot <- function(filename, plot, width=8, height=6) {
 
 main <- function() {
   sc01 <- readRDS(file.path(SCE_CACHE_DIR, 'SC01.rds'))
-  preds <- read.csv(file.path(CACHE_DIR, 'sc01-preds.csv'))
+  sc01.clusters <- read.csv(file.path(METADATA_DIR, 'SC01_clusters.csv'), header=FALSE)
+  sc01.clusters$V1 <- sub("C", "", sc01.clusters$V1)
+  
+  mca.clusters <- read.csv(file.path(CURRENT_DIR, 'MCA_clusters.csv'))
+  
+  preds <- read.csv(file.path(CURRENT_DIR, 'sc01-preds.csv'))
   preds$X0 <- sub("-1", "", preds$X0)
   preds <- merge(sc01@cell.names, preds, by.x="x", by.y="X0")
   sc01 <- StashIdent(sc01, save.name = 'orig')
@@ -24,7 +31,12 @@ main <- function() {
   pdfPlot('sc01-catboost-probs.pdf', function() {
     hist(preds$prob, breaks=100)
   })
-  plot(getSankey(sc01@meta.data$orig, sc01@ident))
+  
+  labels1 <- sc01.clusters$V2[match(sc01@meta.data$orig, sc01.clusters$V1)]
+  labels2 <- mca.clusters$Annotation[match(sc01@ident, mca.clusters$ClusterID)]
+  levels(labels2) <- c(levels(labels2), "Unassigned")
+  labels2[is.na(labels2)] <- "Unassigned"
+  plot(getSankey(labels1, labels2, plot_width = 800, plot_height = 800))
 }
 
 main()
