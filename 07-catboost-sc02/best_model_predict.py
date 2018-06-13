ec59758f43a92124747bc46f7dcd0e20e7525643
Nikolay Markov <a4blank@yahoo.com>
2018-06-13 21:38:43 +0100

    SC02-based model: redo prediction with new old SC02 data

--- 07-catboost-sc02/best_model_predict.py
+++ 07-catboost-sc02/best_model_predict.py
@@ -1,13 +1,9 @@
-import pandas as pd
-import numpy as np
-import scipy.sparse
-from sklearn.model_selection import train_test_split
-from sklearn.model_selection import StratifiedKFold
-from sklearn.model_selection import ParameterGrid
-import catboost
-import sklearn.metrics
-
 import os
+
+import pandas as pd
+import catboost
+
+import utils
 
 
 CUR_DIR = os.path.dirname(os.path.abspath(__file__))
@@ -18,31 +14,6 @@ def path(*args):
     return os.path.join(ROOT, *args)
 
 
-def load_10x(path, batch_label):
-    mtx = pd.read_csv('{}/matrix.mtx'.format(path), skiprows=3, sep=' ', header=None)
-    genes = pd.read_table('{}/genes.tsv'.format(path), header=None, index_col=1)
-    cells = pd.read_table('{}/barcodes.tsv'.format(path), header=None, index_col=0)
-    assgn = pd.read_csv('{}/{}_assgn.csv'.format(
-        os.path.join(CUR_DIR, '..', '01-cluster-sc01-sc02'),
-        batch_label,
-    ), index_col=0)
-    assgn.columns = ['cluster']
-    exp = scipy.sparse.csc_matrix((mtx[2], (mtx[0] - 1, mtx[1] - 1)), shape=(len(genes), len(cells)))
-    exp = pd.SparseDataFrame(exp)
-    exp.columns = cells.index.str.replace('-1', '')
-    exp = exp.transpose()
-    exp.columns = genes.index
-    exp = exp.to_dense()
-    exp['Batch'] = batch_label
-    exp.fillna(0, inplace=True)
-    cols = exp.columns.unique()
-    exp = exp.groupby(level=0, axis=1).sum()
-    exp = exp.reindex(columns=cols)
-    exp = exp.join(assgn)
-    exp = exp[~exp.cluster.isna()]
-    return exp[exp.columns[:-1]], exp.cluster
-
-
 def predict(model, input_columns, experiment):
     missing_columns = input_columns[~ input_columns.isin(experiment.columns)]
     experiment = experiment.copy()
@@ -51,7 +22,7 @@ def predict(model, input_columns, experiment):
 
 
 def main():
-    X, y = load_10x(path('SC02'), 'SC02')
+    X, y = utils.load_10x(path('SC02'), 'SC02')
     best_model = catboost.CatBoostClassifier(
         l2_leaf_reg=3,
         learning_rate=0.445,
@@ -61,7 +32,7 @@ def main():
         logging_level='Silent',
         loss_function='MultiClass',
         eval_metric='TotalF1',
-        #thread_count=20,
+        thread_count=20,
     )
     model_path = os.path.join(CUR_DIR, 'sc02-best-model.cbm')
     if os.path.exists(model_path):
@@ -72,7 +43,7 @@ def main():
         )
         best_model.save_model(model_path)
 
-    sc03, _ = load_10x(path('SC03'), 'SC03')
+    sc03, _ = utils.load_10x(path('SC03'), 'SC03')
     sc03_preds = predict(best_model, X.columns, sc03)
     sc03_preds.to_csv(os.path.join(CUR_DIR, 'sc03-preds.csv'))
 
@@ -80,7 +51,7 @@ def main():
     importances[importances[0] > 0].sort_values(
         0,
         ascending=False
-    ).to_csv('./sc02-model-features.csv')
+    ).to_csv(os.path.join(CUR_DIR, 'sc02-model-features.csv'))
 
 
 if __name__ == '__main__':
--- 07-catboost-sc02/utils.py
+++ 07-catboost-sc02/utils.py
@@ -9,7 +9,8 @@ import os
 
 CUR_DIR = os.path.dirname(os.path.abspath(__file__))
 
-def load_10x(path, batch_label):
+
+def load_10x_scanpy(path, batch_label):
     sc01 = sc.read('{}/matrix.mtx'.format(path), cache=True).T
     sc01.var_names = pd.read_table('{}/genes.tsv'.format(path), header=None)[1]
     sc01.obs_names = pd.read_table('{}/barcodes.tsv'.format(path), header=None)[0]
@@ -17,15 +18,23 @@ def load_10x(path, batch_label):
     sc01.var_names_make_unique()
     sc.pp.filter_cells(sc01, min_genes=200)
     sc.pp.filter_genes(sc01, min_cells=3)
-    exp = pd.DataFrame(sc01.X.todense(), index=sc01.obs_names, columns=sc01.var_names)
-    exp['Batch'] = batch_label
-    exp.fillna(0, inplace=True)
 
     assgn = pd.read_csv('{}/{}_assgn.csv'.format(
         os.path.join(CUR_DIR, '..', '01-cluster-sc01-sc02'),
         batch_label,
     ), index_col=0)
     assgn.columns = ['cluster']
-    exp = exp.join(assgn)
+
+    sc01.obs['cluster'] = assgn.cluster[sc01.obs.index]
+    return sc01
+
+
+def load_10x(path, batch_label):
+    sc01 = load_10x_scanpy(path, batch_label)
+    exp = pd.DataFrame(sc01.X.todense(), index=sc01.obs_names, columns=sc01.var_names)
+    exp['Batch'] = batch_label
+    exp.fillna(0, inplace=True)
+
+    exp['cluster'] = sc01.obs.cluster[exp.index]
     exp = exp[~exp.cluster.isna()]
     return exp[exp.columns[:-1]], exp.cluster
