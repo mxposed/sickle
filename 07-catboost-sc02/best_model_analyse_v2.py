5560edf31af5c906d89fca53625e3a3e4e9a6596
Nikolay Markov <a4blank@yahoo.com>
2018-06-25 13:59:55 +0100

    SC02-based model: plot second maxes v2

--- 07-catboost-sc02/best_model_analyse_v2.py
+++ 07-catboost-sc02/best_model_analyse_v2.py
@@ -40,17 +40,55 @@ def heatmap(predictions, figsize=(16, 20)):
     return fig
 
 
+def get_second_maxes(predictions):
+    second_choices = pd.DataFrame(index=predictions.columns, columns=predictions.columns)
+    second_choices.fillna(0, inplace=True)
+    for cell in predictions.index:
+        cell_type = predictions.loc[cell, :].idxmax()
+        score = predictions.loc[cell, cell_type]
+        second_cell_type = predictions.loc[cell, :][predictions.loc[cell, :] < score].idxmax()
+        second_choices.loc[cell_type, second_cell_type] += 1
+    sums = second_choices.sum(axis=1)
+    sums[sums == 0] = 1
+    second_choices = second_choices.div(sums, axis='index')
+    total = second_choices.sum(axis=0)
+    second_choices = second_choices.reindex_axis(sorted(second_choices.columns, key=lambda x: -total[x]), axis=1)
+    return second_choices
+
+
+def plot_second_maxes(maxes):
+    plt.figure(figsize=(14,14))
+    sums = maxes.sum(axis=0)
+    ax = seaborn.clustermap(maxes,
+                            xticklabels=[maxes.columns[i] + ' ({:.2f})'.format(sums[i]) for i in range(len(sums))],
+                            col_cluster=False,
+                           )
+    ax.ax_heatmap.set_ylabel('Main cell type')
+    ax.ax_heatmap.set_xlabel('Second cell type')
+    return ax
+
+
 def process(exp):
+    sc03_clusters = pd.read_csv('{}/SC03_assgn.csv'.format(
+        os.path.join(CUR_DIR, '..', '01-cluster-sc01-sc02'),
+    ), index_col=0)
+    sc03_clusters.columns = ['cluster']
     preds = load_predictions(os.path.join(CUR_DIR, '{}-preds.csv'.format(exp)))
+
+    second_maxes = get_second_maxes(preds)
+    maxes_heatmap = plot_second_maxes(second_maxes)
+    maxes_heatmap.savefig(os.path.join(CUR_DIR, '{}-second-max-heatmap.png'.format(exp)))
+
+    plasma_second_maxes = get_second_maxes(preds.loc[sc03_clusters.index[sc03_clusters.cluster == 7],:])
+    maxes_heatmap = plot_second_maxes(plasma_second_maxes)
+    plt.suptitle('Second max predictions for Plasma cells')
+    maxes_heatmap.savefig(os.path.join(CUR_DIR, '{}-plasma-second-max-heatmap.png'.format(exp)))
+
     hmap = heatmap(preds)
     hmap.suptitle('Predictions for SC03 dataset')
     hmap.subplots_adjust(top=0.88)
     hmap.savefig(os.path.join(CUR_DIR, '{}-heatmap.png'.format(exp)))
 
-    sc03_clusters = pd.read_csv('{}/SC03_assgn.csv'.format(
-        os.path.join(CUR_DIR, '..', '01-cluster-sc01-sc02'),
-    ), index_col=0)
-    sc03_clusters.columns = ['cluster']
     hmap = heatmap(preds.loc[sc03_clusters.index[sc03_clusters.cluster == 7],:], figsize=(16, 12))
     hmap.suptitle('Predictions for SC03 Plasma cells')
     hmap.subplots_adjust(top=0.88)
