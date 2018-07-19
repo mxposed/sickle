33a62bb4fce834c32f8102d8f811e817fb5f305d
Nikolay Markov <a4blank@yahoo.com>
2018-07-19 15:11:44 +0100

    SC01-based model

---
+++ 10-catboost-sc01/train_predict.py
@@ -0,0 +1,58 @@
+import os
+
+import pandas as pd
+import catboost
+
+import utils
+
+
+CUR_DIR = os.path.dirname(os.path.abspath(__file__))
+ROOT = os.path.dirname(os.path.dirname(CUR_DIR))
+
+
+def path(*args):
+    return os.path.join(ROOT, *args)
+
+
+def predict(model, input_columns, experiment):
+    missing_columns = input_columns[~ input_columns.isin(experiment.columns)]
+    experiment = experiment.copy()
+    experiment[list(missing_columns)] = pd.DataFrame([[0] * len(missing_columns)], index=experiment.index)
+    return pd.DataFrame(model.predict_proba(experiment[input_columns]), index=experiment.index)
+
+
+def main():
+    X, y = utils.load_10x(path('SC02'), 'SC02v2')
+    best_model = catboost.CatBoostClassifier(
+        l2_leaf_reg=7,
+        learning_rate=0.622,
+        depth=10,
+        iterations=200,
+        random_seed=42,
+        logging_level='Silent',
+        loss_function='MultiClass',
+        eval_metric='TotalF1',
+        thread_count=20,
+    )
+    model_path = os.path.join(CUR_DIR, 'sc02v2-best-model.cbm')
+    if os.path.exists(model_path):
+        best_model.load_model(model_path)
+    else:
+        best_model.fit(
+            X, y, [X.shape[1] - 1]
+        )
+        best_model.save_model(model_path)
+
+    sc03, _ = utils.load_10x(path('SC03'), 'SC03')
+    sc03_preds = predict(best_model, X.columns, sc03)
+    sc03_preds.to_csv(os.path.join(CUR_DIR, 'sc03v2-preds.csv'))
+
+    importances = pd.DataFrame(best_model._feature_importance, X.columns)
+    importances[importances[0] > 0].sort_values(
+        0,
+        ascending=False
+    ).to_csv(os.path.join(CUR_DIR, 'sc02v2-model-features.csv'))
+
+
+if __name__ == '__main__':
+    main()
