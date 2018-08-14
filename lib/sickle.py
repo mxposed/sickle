bb7bbc975bf66b6e0161e9c4da30ee60a670b07c
Nikolay Markov <a4blank@yahoo.com>
2018-08-14 18:57:15 +0100

    Common lib files + runner

---
+++ lib/sickle.py
@@ -0,0 +1,81 @@
+import display_settings
+
+import collections
+import os
+
+import pandas as pd
+import sklearn.metrics
+
+
+dirs = {}
+
+def paths(path):
+    dirs['cur'] = os.path.dirname(os.path.abspath(path))
+    dirs['code'] = os.path.dirname(dirs['cur'])
+    dirs['root'] = os.path.dirname(dirs['code'])
+    return dirs['cur'], dirs['root']
+
+
+def sankey_order():
+    return pd.read_csv(os.path.join(dirs['code'], 'sankey_order.csv')).order
+
+
+def assignments(dataset):
+    exp_clusters = pd.read_csv('{}/{}_assgn.csv'.format(
+        os.path.join(dirs['code'], '01-cluster-sc01-sc02'),
+        dataset
+    ), index_col=0)
+    return exp_clusters.iloc[:, 0]
+
+
+class Mapping:
+    def __init__(self, table):
+        self.table = table
+        self.src_mult = set()
+        self.dst_mult = set()
+
+        for name, count in collections.Counter(table['from']).items():
+            if count > 1:
+                self.src_mult.add(name)
+
+        for name, count in collections.Counter(table['to']).items():
+            if count > 1:
+                self.dst_mult.add(name)
+
+        self.src_replace = {}
+        self.dst_replace = {}
+        for idx in table.index:
+            src = table.loc[idx, 'from']
+            dst = table.loc[idx, 'to']
+            if src in self.src_mult:
+                self.dst_replace[dst] = src
+            else:
+                self.src_replace[src] = dst
+
+    def category(self, source, dest):
+        rows_src = self.table[self.table['from'] == source]
+        rows = rows_src[rows_src['to'] == dest]
+        if len(rows) == 0:
+            if len(rows_src) == 1 and rows_src['to'].iloc[0] == 'Novel cell type':
+                return 'novel'
+            return 'mistake'
+        if source in self.src_mult:
+            return 'increase'
+        if dest in self.dst_mult:
+            return 'decrease'
+        return 'correct'
+
+    def f1(self, true, pred):
+        true = true.replace(self.src_replace)
+        pred = pred.replace(self.dst_replace)
+        return sklearn.metrics.f1_score(true, pred, average='weighted')
+
+
+def mapping(query, reference):
+    mapping_file = os.path.join(
+        dirs['code'],
+        '00-metadata',
+        '{}_to_{}.csv'.format(query, reference)
+    )
+    if os.path.exists(mapping_file):
+        return Mapping(pd.read_csv(mapping_file, index_col=None))
---
+++ run.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+PYTHONPATH="./lib/" python "$@"
