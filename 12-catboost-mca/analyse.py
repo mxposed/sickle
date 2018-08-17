81a405b047038c6f48aa38f52640d08e74d167af
Nikolay Markov <a4blank@yahoo.com>
2018-08-18 00:06:58 +0100

    MCA-based model: best params search

---
+++ 12-catboost-mca/analyse.py
@@ -0,0 +1,117 @@
+import sickle
+
+import os
+
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn
+
+import sankey
+import utils
+
+
+CUR_DIR, ROOT = sickle.paths(__file__)
+
+
+def heatmap(predictions, figsize=(16, 20)):
+    preds = predictions.copy()
+    preds.columns = list(range(len(preds.columns)))
+    preds['cluster'] = preds.idxmax(axis=1)
+    preds['max_score'] = predictions.max(axis=1)
+    preds.sort_values(['cluster', 'max_score'], ascending=[True, False], inplace=True)
+    preds = predictions.reindex(preds.index)
+    plt.figure(figsize=figsize)
+    ax = seaborn.heatmap(preds, yticklabels=[])
+    fig = ax.get_figure()
+    fig.tight_layout()
+    return fig
+
+
+def get_second_maxes(predictions):
+    second_choices = pd.DataFrame(index=predictions.columns, columns=predictions.columns)
+    second_choices.fillna(0, inplace=True)
+    for cell in predictions.index:
+        cell_type = predictions.loc[cell, :].idxmax()
+        score = predictions.loc[cell, cell_type]
+        second_cell_type = predictions.loc[cell, :][predictions.loc[cell, :] < score].idxmax()
+        second_choices.loc[cell_type, second_cell_type] += 1
+    sums = second_choices.sum(axis=1)
+    sums[sums == 0] = 1
+    second_choices = second_choices.div(sums, axis='index')
+    total = second_choices.sum(axis=0)
+    second_choices = second_choices.reindex(columns=sorted(second_choices.columns, key=lambda x: -total[x]))
+    return second_choices
+
+
+def plot_second_maxes(maxes):
+    plt.figure(figsize=(14,14))
+    sums = maxes.sum(axis=0)
+    ax = seaborn.clustermap(maxes,
+                            xticklabels=[maxes.columns[i] + ' ({:.2f})'.format(sums[i]) for i in range(len(sums))],
+                            col_cluster=False,
+                           )
+    ax.ax_heatmap.set_ylabel('Main cell type')
+    ax.ax_heatmap.set_xlabel('Second cell type')
+    return ax
+
+
+def process(exp, reference, query):
+    exp_clusters = sickle.assignments(query)
+
+    clusters = pd.read_csv('{}/{}_clusters.csv'.format(
+        os.path.join(os.path.dirname(CUR_DIR), '00-metadata'),
+        query
+    ), index_col=0, header=None)
+    clusters.index = clusters.index.str.replace('C', '').astype(int)
+
+    preds = utils.load_predictions(
+        os.path.join(CUR_DIR, '{}-preds.csv'.format(exp)),
+        reference
+    )
+
+    second_maxes = get_second_maxes(preds)
+    maxes_heatmap = plot_second_maxes(second_maxes)
+    maxes_heatmap.savefig(os.path.join(CUR_DIR, '{}-second-max-heatmap.png'.format(exp)))
+
+    if query == 'SC03':
+        plasma_second_maxes = get_second_maxes(preds.loc[exp_clusters.index[exp_clusters == 7],:])
+        maxes_heatmap = plot_second_maxes(plasma_second_maxes)
+        plt.suptitle('Second max predictions for Plasma cells')
+        maxes_heatmap.savefig(os.path.join(CUR_DIR, '{}-plasma-second-max-heatmap.png'.format(exp)))
+
+    hmap = heatmap(preds)
+    hmap.suptitle('Predictions for {} dataset'.format(query))
+    hmap.subplots_adjust(top=0.88)
+    hmap.savefig(os.path.join(CUR_DIR, '{}-heatmap.png'.format(exp)))
+
+    if query == 'SC03':
+        hmap = heatmap(preds.loc[exp_clusters.index[exp_clusters == 7],:], figsize=(16, 12))
+        hmap.suptitle('Predictions for SC03 Plasma cells')
+        hmap.subplots_adjust(top=0.88)
+        hmap.savefig(os.path.join(CUR_DIR, '{}-plasma-heatmap.png'.format(exp)))
+
+    mapping = sickle.mapping(query, reference)
+    s = sankey.sankey(
+        clusters.iloc[:, 0].loc[exp_clusters],
+        preds.idxmax(axis=1),
+        alpha=.7,
+        left_order=sickle.sankey_order(),
+        mapping=mapping
+    )
+    s.savefig(os.path.join(CUR_DIR, '{}-sankey.png'.format(exp)))
+
+    if mapping:
+        open(os.path.join(CUR_DIR, '{}-f1.txt'.format(exp)), 'w').write(
+            'F1 score: {:.4f}'.format(
+                mapping.f1(clusters.iloc[:, 0].loc[exp_clusters],
+                           preds.idxmax(axis=1))
+            )
+        )
+
+
+def main():
+    process('sc02', 'SC01v2', 'SC02v2')
+
+
+if __name__ == '__main__':
+    main()
---
+++ 12-catboost-mca/params_search.py
@@ -0,0 +1,105 @@
+import sickle
+
+import os
+import sys
+
+import catboost
+import numpy as np
+import pandas as pd
+import sklearn.model_selection
+import sklearn.metrics
+import timeit
+
+
+CUR_DIR, ROOT = sickle.paths(__file__)
+
+
+def cross_val(X, y, params, cv=5):
+    skf = sklearn.model_selection.StratifiedKFold(
+        n_splits=cv,
+        shuffle=True,
+        random_state=42
+    )
+
+    scores = []
+    splits = list(skf.split(X, y))
+    for i, (tr_ind, val_ind) in enumerate(splits):
+        print('({}/{})'.format(i + 1, len(splits)), file=sys.stderr, end='')
+        sys.stderr.flush()
+
+        X_train, y_train = X.iloc[tr_ind, :], y.iloc[tr_ind]
+        X_valid, y_valid = X.iloc[val_ind, :], y.iloc[val_ind]
+
+        clf = catboost.CatBoostClassifier(
+            l2_leaf_reg=params['l2_leaf_reg'],
+            learning_rate=params['learning_rate'],
+            depth=params['depth'],
+            iterations=50,
+            random_seed=42,
+            logging_level='Silent',
+            loss_function='MultiClass',
+            eval_metric='TotalF1',
+            thread_count=20,
+        )
+
+        clf.fit(
+            X_train,
+            y_train,
+            cat_features=[X.shape[1] - 1],
+        )
+
+        y_pred = clf.predict(X_valid)
+        score = sklearn.metrics.f1_score(y_valid, y_pred, average='weighted')
+        scores.append(score)
+        print('\b\b\b\b\b', file=sys.stderr, end='')
+        sys.stderr.flush()
+    return np.mean(scores)
+
+
+def catboost_GridSearchCV(X, y, params_space, record, cv=5, splits=1, current_split=1):
+    max_score = 0
+    best_params = None
+    all_params = list(sklearn.model_selection.ParameterGrid(params_space))
+    divider = current_split - 1
+    my_params = [all_params[i] for i in range(len(all_params)) if i % splits == divider]
+    for i, params in enumerate(my_params):
+        print('{:3d}% '.format(i * 100 // len(my_params)), file=sys.stderr, end='')
+        sys.stderr.flush()
+        start = timeit.default_timer()
+        score = cross_val(X, y, params, cv=cv)
+        if score > max_score:
+            max_score = score
+            best_params = params
+        record.append([repr(params), score, timeit.default_timer() - start])
+        print('\r' + ' ' * 20 + '\r', file=sys.stderr, end='')
+        sys.stderr.flush()
+    return max_score, best_params
+
+
+def main(splits, current_split):
+    X, y = sickle.load_mca_lung('MCAv2')
+    params_space = {
+        'l2_leaf_reg': [1, 3, 5, 7, 9],
+        'learning_rate': np.linspace(1e-3, 8e-1, num=10),
+        'depth': [6, 8, 10],
+    }
+    record = []
+    score, best_params = catboost_GridSearchCV(X, y, params_space,
+                                               record, cv=5,
+                                               splits=splits,
+                                               current_split=current_split)
+    pd.DataFrame(record, columns=['params', 'score', 'time']).to_csv(
+        os.path.join(
+            CUR_DIR,
+            'search-{}-of-{}.csv'.format(current_split, splits)
+        )
+    )
+
+
+if __name__ == '__main__':
+    if len(sys.argv) < 3:
+        print('Usage: {} <N> <K>'.format(__file__), file=sys.stderr)
+        sys.exit(1)
+    splits = int(sys.argv[1])
+    my_split = int(sys.argv[2])
+    main(splits, my_split)
