00a95df364cb6c74aa80e82205afe2f18142a0e9
Nikolay Markov <a4blank@yahoo.com>
2018-09-04 18:01:44 +0300

    Add ipython to run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
 dir=$(dirname "$0")
-PYTHONPATH="$dir/lib/" python3.6 "$@"
+if [ "$1" = "shell" ]; then
+    PYTHONPATH="$dir/lib/" ipython
+else
+    PYTHONPATH="$dir/lib/" python3.6 "$@"
+fi
