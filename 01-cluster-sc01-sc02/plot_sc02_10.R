17da5948a3ce2cf44d6caa9beb42e2c51bde1386
Nikolay Markov <a4blank@yahoo.com>
2018-09-06 00:43:37 +0300

    Cluster scripts: uncomment, prettify

--- 01-cluster-sc01-sc02/plot_sc02_10.R
+++ 01-cluster-sc01-sc02/plot_sc02_10.R
@@ -27,10 +27,10 @@ pdfPlot <- function(filename, plot, width=8, height=6) {
   return(result)
 }
 
-zz <- function(sc02) {
+plotHeatmap <- function(sc02) {
   p <- DoHeatmap(
-    sc02, 
-    genes.use = c("Irf8", "Cd209a", "Itgae", "Ccr7"), 
+    sc02,
+    genes.use = c("Irf8", "Cd209a", "Itgae", "Ccr7"),
     cells.use = WhichCells(sc02, 10),
     slim.col.label = TRUE,
     group.by = NULL,
@@ -44,27 +44,27 @@ zz <- function(sc02) {
   p$data$cell <- factor(p$data$cell, levels = custom_order)
   p <- p + labs(x="Cells from cluster #10 in SC02", tag="B") +
     theme(
-      axis.title.x = element_text(size=14), 
+      axis.title.x = element_text(size=14),
       axis.text.y = element_text(size=14),
       plot.tag.position = "topright",
       plot.tag = element_text(size = 20)
     ) + guides(fill=guide_colorbar(
-      barheight=7.8, 
-      title = "Gene expression z-score", 
-      title.position = "right", 
+      barheight=7.8,
+      title = "Gene expression z-score",
+      title.position = "right",
       title.theme = element_text(angle=90, size = 10),
       label.theme = element_text(size=8, angle = 0),
       title.vjust = 0.5
-    )) + 
+    )) +
     geom_tile(colour=NA)
   plot(p)
 }
 
-qq <- function(sc02) {
+plotTsne <- function(sc02) {
   p <- TSNEPlot(
-    sc02, 
-    do.label = TRUE, 
-    do.return = TRUE, 
+    sc02,
+    do.label = TRUE,
+    do.return = TRUE,
     pt.size = 0.1,
     label.size = 3.5
   )
@@ -89,13 +89,15 @@ qq <- function(sc02) {
 main <- function() {
   sc02 <- readRDS(file.path(SCE_CACHE_DIR, 'SC02v2.rds'))
 
-  pdfPlot('sc02_cluster10.pdf', function() {
-    zz(sc02)
-  }, width = 5, height = 2)
-  
   pdfPlot('sc02v2_tsne_final.pdf', function() {
-    qq(sc02)
+    plotTsne(sc02)
   }, width = 5, height = 4)
+
+  pdfPlot('sc02_cluster10.pdf', function() {
+    plotHeatmap(sc02)
+  }, width = 5, height = 2)
+
+
 }
 
 main()
--- 01-cluster-sc01-sc02/sc02.R
+++ 01-cluster-sc01-sc02/sc02.R
@@ -31,7 +31,7 @@ cluster <- function(dataset, save_as=NULL, num_pcs=NULL, resolution=0.5) {
     save_as <- dataset
   }
   cache_file <- paste(CURRENT_DIR, paste(save_as, "rds", sep='.'), sep='/')
-  
+
   if (file.exists(cache_file)) {
     sce <- readRDS(cache_file)
   } else {
@@ -40,8 +40,8 @@ cluster <- function(dataset, save_as=NULL, num_pcs=NULL, resolution=0.5) {
     mito.genes <- grep(pattern="^mt-", x=rownames(sce@data), value=TRUE)
     percent.mito <- Matrix::colSums(sce@raw.data[mito.genes, ])/Matrix::colSums(sce@raw.data)
     sce <- AddMetaData(sce, metadata=percent.mito, col.name="percent.mito")
-    sce <- FilterCells(sce, subset.names = c("nGene", "percent.mito"), 
-                       low.thresholds = c(300, -Inf), 
+    sce <- FilterCells(sce, subset.names = c("nGene", "percent.mito"),
+                       low.thresholds = c(300, -Inf),
                        high.thresholds = c(4000, 0.1))
     sce <- NormalizeData(sce)
     sce <- ScaleData(sce, vars.to.regress = c("nUMI", "percent.mito"))
@@ -49,43 +49,43 @@ cluster <- function(dataset, save_as=NULL, num_pcs=NULL, resolution=0.5) {
     sce <- RunPCA(sce, pc.genes = sce@var.genes, do.print = FALSE, pcs.compute = 40)
     sce <- ProjectPCA(sce, do.print=FALSE)
   }
-  
+
   pdfPlot(paste0(save_as, '_elbow.pdf'), function() {
     plot(PCElbowPlot(sce, num.pc = 40))
   })
-  
+
   sce <- RunTSNE(sce, dims.use = 1:num_pcs, check_duplicates = FALSE, do.fast=TRUE)
-  sce <- FindClusters(sce, dims.use = 1:num_pcs, resolution=resolution, 
+  sce <- FindClusters(sce, dims.use = 1:num_pcs, resolution=resolution,
                       print.output = FALSE, save.SNN = TRUE, force.recalc = TRUE)
-  
+
   #sce <- SetIdent(sce, cells.use = WhichCells(sce, ident=3), 0)
   sce <- ValidateSpecificClusters(sce, 2, 0)
   ident <- as.numeric(levels(sce@ident)[sce@ident])
   ident[ident > 2] <- ident[ident > 2] - 1
   sce <- SetIdent(sce, ident.use = ident)
-  
+
   write.csv(sce@ident, file.path(CURRENT_DIR, paste0(save_as, '_assgn.csv')))
-  
+
   pdfPlot(paste0(save_as, '_tsne.pdf'), function() {
     TSNEPlot(sce, do.label = TRUE)
   }, width=12, height=10)
-  
+
   saveRDS(sce, cache_file)
   return(sce)
 }
 
 markers <- function(sce, save_as, num_pcs, resolution) {
-  markers <- FindAllMarkers(sce, only.pos = TRUE, 
-                            min.pct = 0.25, thresh.use = 0.5, 
+  markers <- FindAllMarkers(sce, only.pos = TRUE,
+                            min.pct = 0.25, thresh.use = 0.5,
                             max.cells.per.ident = 200)
-  markers_file <- file.path(CURRENT_DIR, 
+  markers_file <- file.path(CURRENT_DIR,
                             paste0(save_as, "_markers_PC1-", num_pcs, "res", resolution, ".csv"))
   write.csv(markers, markers_file)
 }
 
 main <- function() {
   sc02 <- cluster("SC02", save_as="SC02v2", num_pcs = 25, resolution = 0.5)
-  #markers(sc02, "SC02v2", num_pcs = 25, resolution = 0.5)
+  markers(sc02, "SC02v2", num_pcs = 25, resolution = 0.5)
 }
 
 main()
